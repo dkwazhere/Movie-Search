4d63c2f7d5f6cb24f33948358e54e4491232031c
dkwazhere <dhealth77@gmail.com>
2018-10-01 08:16:27 -0700

    added javascript, connected js with omdb api using axios, display each movie img, title, and append button

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// Using es6 function
+$(document).ready(() => {
+  $("#searchForm").on("submit", e => {
+    let searchText = $("#searchText").val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+function getMovies(searchText) {
+  console.log(searchText);
+  axios
+    .get("http://www.omdbapi.com?s=" + searchText + "&apikey=8dec85ac")
+    .then(response => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = "";
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected"('${
+                movie.imdbID
+              }')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+      $("#movies").html(output);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
